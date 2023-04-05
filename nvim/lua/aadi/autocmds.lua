0353da832f891caefe3a23077102538c961694ef
aaditeynair <aaditeyn@gmail.com>
2023-04-05 12:47:29 +0530

    move CWD based session load function from autocmds.lua to utils.lua

--- nvim/lua/aadi/autocmds.lua
+++ nvim/lua/aadi/autocmds.lua
@@ -17,14 +17,3 @@ vim.api.nvim_create_autocmd("TermOpen", {
         })
     end,
 })
-
-vim.api.nvim_create_user_command("SLoadCwd", function()
-    local cwd = vim.fn.getcwd()
-    local sessions = require("possession.session").list()
-    for _, session in pairs(sessions) do
-        if cwd == session.cwd then
-            require("possession.session").load(session.name)
-            break
-        end
-    end
-end, { nargs = 0 })
